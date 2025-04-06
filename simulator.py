6242bf26b0389acded3babe42fac71c31a9753c2
Jasper Niittyvuopio <jasper.niittyvuopio@hotmail.com>
2025-04-06 22:48:28 +0300

    features and comments added

--- simulator.py
+++ simulator.py
@@ -1,12 +1,13 @@
 from random import choices
 import pandas as pd
+import numpy as np
 
 
+#flags
 enable_anti_martingale = False
 enable_trades_csv = False
 
-population = [0, 1]
-weights = []
+#user input
 
 anti_martingale = ""
 while anti_martingale not in ["y", "n"]:
@@ -52,11 +53,28 @@ if not result_csv_name.strip():
     result_csv_name = "results"
 
 
+#random number generation
+
+population = [0, 1]
+weights = []
+
 lossrate = 1 - winrate
 weights.append(lossrate)
 weights.append(winrate)
 
 #csv-formats
+
+metadata = {
+    "anti_martingale" : [],
+    "winrate" : [],
+    "start_balance" : [],
+    "initial_risk" : [],
+    "hops" : [],
+    "wins_to_skip" : [],
+    "risk_multiplier" : [],
+    "test_cycles" : [],
+    "trades_per_cycle" : [],
+}
 
 data_results= {
     "end_balance" : [],
@@ -69,24 +87,35 @@ data_results= {
     "max_loss_streak" : [],
     "wins" : [],
     "losses" : [],
-    "winrate" : []
+    "winrate" : [],
+    "biggest_bankrun" : [],
+    "average_bankrun" : []
 }
 
 if enable_trades_csv:
     data_trades= {
-                "balance" : [],
-                "result" : [],
-                "win_amount" : [],
-                "loss_amount" : [],
-                "max_drawdown" : [], 
-                "winrate" : [],
-                "chicken_dinners/win_streak" : [],
-                "loss_streak" : []
+        "balance" : [],
+        "result" : [],
+        "win_amount" : [],
+        "loss_amount" : [],
+        "max_drawdown" : [], 
+        "winrate" : [],
+        "chicken_dinners/win_streak" : [],
+        "loss_streak" : []
     }
 
+#create dataframes
+metadata_csv = pd.DataFrame(data=metadata)
 result_csv = pd.DataFrame(data=data_results)
 
-#enumarate generated test cycle csv's
+#create metadata file
+metadata_input = [enable_anti_martingale, winrate, start_balance, initial_risk, hops, to_skip, multiplier, n_cycles, n_trades]
+metadata_csv.loc[len(metadata_csv)] = metadata_input
+
+metadata_filename = result_csv_name + "_metadata" + ".csv"
+metadata_csv.to_csv(metadata_filename)
+
+#enumarate generated test cycle files
 if enable_trades_csv:
     increment = 0
 
@@ -94,11 +123,15 @@ while n_cycles > 0:
     
     n_cycles -= 1
     
+    #test cycle initiation
+    
     trades = choices(population, weights, k=n_trades)
     
     balance = start_balance
     pot = balance * initial_risk
     win_streak = 0
+    bank = 0
+    bank_runs = []
     loss_streak = 0
     
     chicken_dinners = 0
@@ -112,6 +145,7 @@ while n_cycles > 0:
     
     if enable_trades_csv:
         
+        #create test cycle dataframes and enumare them
         increment += 1
         filename = trades_csv_name + str(increment) + ".csv"
         
@@ -132,11 +166,14 @@ while n_cycles > 0:
                     max_drawdown = drawdown
                     
             #resets
+            
             if enable_anti_martingale:
                 pot = balance * initial_risk
                 if (hops - win_streak) == 1:
                     runner_ups += 1
+                    
             win_streak = 0
+            bank = 0
             loss_streak += 1
             if loss_streak > max_loss_streak:
                 max_loss_streak = loss_streak
@@ -144,10 +181,20 @@ while n_cycles > 0:
             if enable_trades_csv:
                 winrate = wins / total_trades
                 winrate = winrate * 100
-                loss_trade = [round(balance), "loss", 0, round(loss_amount), round(max_drawdown), round(winrate, 1), chicken_dinners, loss_streak]
+                #log trade into test cycle file
+                loss_trade = [round(balance), 
+                              "loss", 
+                              0, 
+                              round(loss_amount), 
+                              round(max_drawdown), 
+                              round(winrate, 1), 
+                              chicken_dinners, 
+                              loss_streak]
+                
                 trades_csv.loc[len(trades_csv)] = loss_trade
                 
             if balance <= 0:
+                balance = 0
                 break
         
         elif trade == 1:
@@ -157,12 +204,15 @@ while n_cycles > 0:
             loss_streak = 0
             win_amount = pot
             balance = balance + win_amount
+            bank += win_amount
             
             if enable_anti_martingale:
                 if win_streak == hops:
                     pot = balance * initial_risk
                     chicken_dinners += 1
                     win_streak = 0
+                    bank_runs.append(bank)
+                    bank = 0
                 elif win_streak > to_skip:
                     pot = pot * multiplier
             else:
@@ -172,10 +222,20 @@ while n_cycles > 0:
             if enable_trades_csv:
                 winrate = wins / total_trades
                 winrate = winrate * 100
-                win_trade = [round(balance), "win", round(win_amount), 0, round(max_drawdown), round(winrate, 1), chicken_dinners, 0]
+                #log trade into test cycle file
+                win_trade = [round(balance), 
+                             "win", 
+                             round(win_amount), 
+                             0, 
+                             round(max_drawdown), 
+                             round(winrate, 1), 
+                             chicken_dinners,
+                             0]
+                
                 trades_csv.loc[len(trades_csv)] = win_trade
     
     if enable_trades_csv:        
+        #write test cycle to file
         trades_csv.to_csv(filename)
     
     losses = total_trades - wins
@@ -188,8 +248,31 @@ while n_cycles > 0:
     
     max_dd_percentage = (max_drawdown / start_balance) * 100
     
-    new_cycle = [round(balance), round(gain, 1), round(pnl, 1), round(max_drawdown), round(max_dd_percentage, 1), chicken_dinners, runner_ups, max_loss_streak, wins, losses, round(winrate, 1)]
+    max_bank_run = 0
+    mean_bank_run = 0
+    
+    if bank_runs:
+        max_bank_run = np.max(bank_runs)
+        mean_bank_run = np.mean(bank_runs)
+    
+    #write cycle details to test cycle file
+    new_cycle = [round(balance), 
+                 round(gain, 1), 
+                 round(pnl, 1), 
+                 round(max_drawdown), 
+                 round(max_dd_percentage, 1), 
+                 chicken_dinners, 
+                 runner_ups, 
+                 max_loss_streak, 
+                 wins, 
+                 losses, 
+                 round(winrate, 1), 
+                 round(max_bank_run, 1), 
+                 round(mean_bank_run, 1)]
+    
     result_csv.loc[len(result_csv)] = new_cycle
 
+
+#write results to file
 filename = result_csv_name + ".csv"
 result_csv.to_csv(filename)
